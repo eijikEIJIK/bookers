ae5fbb8912a051aa5ba1d72571844d5714cc8b42
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-04-29 13:34:40 +0000

    add edit and destroy func

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,19 +2,17 @@ class BooksController < ApplicationController
 
   def index
     @books=Book.all
-  end
-
-  def new
     @book=Book.new()
   end
 
   def create
-    book = Book.new(list_params)
+    book = Book.new(book_params)
     book.save
-    redirect_to '/'
+    redirect_to '/books'
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def show
@@ -22,14 +20,20 @@ class BooksController < ApplicationController
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
 
   private
 
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 
