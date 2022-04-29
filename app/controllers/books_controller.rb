b7444d244f4eab3cca1fa409c8e2b48552180f2c
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-04-29 15:45:28 +0000

    add flash message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,10 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    if book.save
+      flash[:notice]="Book was successfully created."
+    end
+    redirect_to book_path(book.id)
   end
 
   def edit
@@ -21,14 +23,17 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+    end
     redirect_to book_path(book.id)
   end
 
   def destroy
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice]="Book was successfully destroyed."
+    redirect_to '/books'
   end
 
   private
