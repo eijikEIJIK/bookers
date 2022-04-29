597e2e96c8cbe41f2f1a593045e976a6fc89ea6f
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-04-29 12:57:20 +0000

    add post page, index page and detail page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,36 @@
 class BooksController < ApplicationController
+
+  def index
+    @books=Book.all
+  end
+
+  def new
+    @book=Book.new()
+  end
+
+  def create
+    book = Book.new(list_params)
+    book.save
+    redirect_to '/'
+  end
+
+  def edit
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
