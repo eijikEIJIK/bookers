c9215b91f7711056359234ee6061b47594b547bc
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-05-01 11:20:10 +0000

    add validation and error message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
 
   def index
-    @books=Book.all
-    @book=Book.new()
+    @books = Book.all
+    @book = Book.new()
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
       flash[:notice]="Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :index
     end
-    redirect_to book_path(book.id)
   end
 
   def edit
@@ -22,11 +25,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
     end
-    redirect_to book_path(book.id)
   end
 
   def destroy
